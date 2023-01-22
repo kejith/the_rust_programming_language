972f2ec4eda0e218298e415b734a1d0778b5cb84
kejith <kejith@web.de>
2023-01-22 13:45:37 +0100

    Chapter 3.3 Expressions and Statements

--- Chapter 3/functions/src/main.rs
+++ Chapter 3/functions/src/main.rs
@@ -1,7 +1,20 @@
 fn main() {
-    another_function(5);
+    print_labeled_measurement(5, 'h');
+    codeblocks_are_expressions()
 }
 
-fn another_function(x: i32) {
-    println!("The Value of x is: {x}");
+fn print_labeled_measurement(x: i32, unit_label: char) {
+    println!("The measurement is: {x}{unit_label}");
+}
+
+fn codeblocks_are_expressions() {
+    let y = {
+        let x = 3;
+        // expressions do not include an ending semicolon
+        // if you would use a semicolon here the expression
+        // would turn into a statement and won't return its value
+        x + 1 
+    };
+
+    println!("The value of y is: {y}");
 }
